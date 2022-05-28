4fe586396caac4d093b5141cb039dedee9800b34
SamEdgysCas <105398874+SamEdgysCas@users.noreply.github.com>
2022-05-28 14:33:45 +0800

    Update final nani.py

---
+++ final nani.py
@@ -0,0 +1,22 @@
+#Sam was here
+
+import imp
+from tkinter import *
+import sys
+import time
+global count
+from tkinter import filedialog
+from PIL import ImageTk, Image
+count = 0
+
+class stopwatch():
+
+#LABOS - START/STOP FUNCTION
+
+#JADE - CLOSE / HALF SA TIMER
+
+#CLYDE - HALF SA TIMER / LAP'
+
+#SAM THE GREATEST SEXY MAN - FUNCTION FOR CUSTOM BG / WINDOWS FOR BUTTONS
+
+#OMAR - BUTTONS/ REST OF INNIT ASIDE FROM BUTTON WINDOWS
